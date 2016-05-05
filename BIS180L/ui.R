f30c0966b655b251db548630d6b45d4e91fa89a6
fmacaran <fmacarandang@ucdavis.edu>
2016-05-05 16:32:42 -0700

    Update ui.R

--- BIS180L/ui.R
+++ BIS180L/ui.R
@@ -21,12 +21,6 @@ shinyUI(bootstrapPage(
 
   
   plotOutput(outputId = "mainPlot", height = "300px"),
-  
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-                   sliderInput(inputId = "bw_adjust",
-                               label = "Bandwidth adjustment:",
-                               min = 0.2, max = 2, value = 1, step = 0.2)
-  )
+
   
 ))
