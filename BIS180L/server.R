6c1f8659f0e8eb345da09f30e919cd48f7a94254
bis180l_local <gwelliott@ucdavis.edu>
2016-05-05 14:22:10 -0700

    Added consolidated code

--- BIS180L/server.R
+++ BIS180L/server.R
@@ -1,14 +1,40 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(ggplot2)
+library(PSMix)
+library(reshape2)
+setwd("~/Shiny_Garret-Francesca-Lucy/RiceSNPData")
+data.geno <- read.csv("Rice_44K_genotypes.csv.gz",
+                      row.names=1,
+                      na.strings=c("NA","00"))
+data.geno.2500 <- data.geno[sample(ncol(data.geno), 2500)]
+geno.numeric <- data.matrix(data.geno.2500)
+genDist <- as.matrix(dist(geno.numeric))
+geno.mds <- as.data.frame(cmdscale(genDist))
+data.pheno<- read.csv("RiceDiversity.44K.MSU6.Phenotypes.csv",
+                      row.names=1, 
+                      na.strings=c("NA","00"))
+data.pheno.mds <- merge(geno.mds, data.pheno, by="row.names")
+data.geno.2500.c <- apply(data.geno.2500,2,as.character)
+data.geno.2500.ps <- matrix("",nrow=nrow(data.geno.2500.c)*2,ncol=ncol(data.geno.2500.c))
+for (i in 1:nrow(data.geno.2500.c)) {
+  data.geno.2500.ps[(i-1)*2+1,] <- substr(data.geno.2500.c[i,],1,1)
+  data.geno.2500.ps[(i-1)*2+2,] <- substr(data.geno.2500.c[i,],2,2)
+}
+#Psmix
+load("ps4.2500.RData")
+ps4.df <- as.data.frame(cbind(round(ps4$AmPr,3),ps4$AmId))
+colnames(ps4.df) <- c(paste("pop",1:(ncol(ps4.df)-1),sep=""),"popID")
+maxGenome <- apply(ps4$AmPr,1,max)
+ps4.df <- ps4.df[order(ps4.df$popID,-maxGenome),]
+ps4.df$sampleID <- factor(1:413)
+ps4.df.melt <- melt(ps4.df,id.vars=c("popID","sampleID"))
+geno.mds$popID <- factor(ps4$AmId)
+colnames(ps4$AmPr) <- paste("pr",1:4,sep="")
+geno.mds <- cbind(geno.mds,ps4$AmPr)
+shinydata <- merge(geno.mds, data.pheno, by="row.names")
 
+#put one time calculations here
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
