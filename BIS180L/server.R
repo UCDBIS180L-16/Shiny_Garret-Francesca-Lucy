7d74ab9babef3130e883e97d65f06b37657a3e55
Garrett-Elliott <gwelliott@ucdavis.edu>
2016-05-12 13:43:48 -0700

    Update

--- BIS180L/server.R
+++ BIS180L/server.R
@@ -1,33 +1,39 @@
-
 library(shiny)
 library(ggplot2)
 library(PSMix)
 library(reshape2)
-  #make sure to place RiceSNPData within BIS180L and make sure correct directory is set
-#these lines were used to create the shinydata.Rdata file 
- # load("RiceSNPData/data_from_SNP_lab.Rdata")
-  #   load("RiceSNPData/ps4.2500.RData")
-  #   ps4.df <- as.data.frame(cbind(round(ps4$AmPr,3),ps4$AmId))
-  #   colnames(ps4.df) <- c(paste("pop",1:(ncol(ps4.df)-1),sep=""),"popID")
-  #   maxGenome <- apply(ps4$AmPr,1,max)
-  #   ps4.df <- ps4.df[order(ps4.df$popID,-maxGenome),]
-  #   ps4.df$sampleID <- factor(1:413)
-  #   ps4.df.melt <- melt(ps4.df,id.vars=c("popID","sampleID"))
-  #   geno.mds$popID <- factor(ps4$AmId)
-  #   colnames(ps4$AmPr) <- paste("pr",1:4,sep="")
-  #   geno.mds <- cbind(geno.mds,ps4$AmPr)
-  #   shinydata <- merge(geno.mds, data.pheno, by="row.names")
-  #   
-  #   save(shinydata,file="shinydata.Rdata")
-    
+#make sure to place RiceSNPData within BIS180L and make sure correct directory is set
+#also make sure this chunk of code is run beforehand
+#load("RiceSNPData/data_from_SNP_lab.Rdata")
+#load("RiceSNPData/ps4.2500.RData")
+#ps4.df <- as.data.frame(cbind(round(ps4$AmPr,3),ps4$AmId))
+#colnames(ps4.df) <- c(paste("pop",1:(ncol(ps4.df)-1),sep=""),"popID")
+#maxGenome <- apply(ps4$AmPr,1,max)
+#ps4.df <- ps4.df[order(ps4.df$popID,-maxGenome),]
+#ps4.df$sampleID <- factor(1:413)
+#ps4.df.melt <- melt(ps4.df,id.vars=c("popID","sampleID"))
+#geno.mds$popID <- factor(ps4$AmId)
+#colnames(ps4$AmPr) <- paste("pr",1:4,sep="")
+#geno.mds <- cbind(geno.mds,ps4$AmPr)
+#shinydata <- merge(geno.mds, data.pheno, by="row.names")
+#shinydata$pr1 <- NULL
+#shinydata$pr2 <- NULL
+#shinydata$pr3 <- NULL
+#shinydata$pr4 <- NULL
+#shinydata$pr1 <- NULL
+#shinydata$pr2 <- NULL
+#shinydata$pr3 <- NULL
+#shinydata$pr4 <- NULL
+#removing duplicated pr values, not necessary
+#save(shinydata, file="shinydata.Rdata")
 load("shinydata.Rdata")
-    
 shinyServer(function(input, output) {
- 
-     output$mainPlot <- renderPlot({
-       pl <- ggplot(data = shinydata, aes(x=V1, y=V2)) + geom_point(aes_string(color=input$trait))
-       pl                        
+  
+  output$mainPlot <- renderPlot({
+    pl <- ggplot(data = shinydata, aes(x=V1, y=V2)) + geom_point(aes_string(color=input$trait), size=I(as.numeric(input$adjust)))
+    pl                        
     
   })
   
 })
+
--- BIS180L/ui.R
+++ BIS180L/ui.R
@@ -6,15 +6,18 @@ load("shinydata.Rdata")
 
 shinyUI(bootstrapPage(
   
-  selectInput("trait","Traits:",
-              choices = colnames(shinydata),
-              selected = "Region"),
-  
 
   titlePanel("Rice SNP Data"),
-  helpText("This application creates a scatter plot using Euclidean distances for a selection of 2500 single-nucleotide polymorphisms (SNPs) for rice. It also colorizes based on a selected characteristic"),
+  helpText("This application creates a scatter plot using Euclidean distances for a selection of 2500 single-nucleotide polymorphisms (SNPs) for rice. It also colorizes based on a selected characteristic."),
   mainPanel(
-  plotOutput(outputId = "mainPlot", height = "300px")
+  plotOutput(outputId = "mainPlot", height = "300px"),
+  helpText("Each SNP has been clustered into one of four populations. Take note of which characteristics are indicative of which population. Data points that are not colorized according to the provided legend are data where the characteristic is not applicable."),
+  selectInput("trait","Traits:",
+              choices = c("popID", "Region", "Country_of_Origin", "Seed.color","Pericarp.color"),
+              selected = "popID"),
+  sidebarPanel( sliderInput("adjust",
+              "Size of Data Points:",
+              min = 0.5,  max = 10, value = 3))
 )
   
 ))
